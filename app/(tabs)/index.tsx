02241295ac50e04d70306ddf9b03434139452ebf
kshetritej <tejghartikshetri@gmail.com>
2024-11-03 22:07:52 +0545

    add: image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,36 @@
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import { Text, View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
 const imgSrc = require('@/assets/images/background-image.png');
+
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <>
       <View style={styles.container}
       >
         <View style={styles.imageContainer}>
-          <ImageViewer imageSource={imgSrc} />
+          <ImageViewer imageSource={imgSrc} selectedImage={selectedImage}  />
         </View>
         <View style={styles.footerContainer}>
-          <Button theme="primary" label="Choose a photo" />
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
           <Button label="Use this photo" />
         </View>
       </View>
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,16 +1,21 @@
 import { View, StyleSheet } from "react-native";
 import { Image, ImageSource } from "expo-image"
 
-export default function ImageViewer({ imageSource }: { imageSource: ImageSource }) {
+type Props = {
+    imageSource: ImageSource;
+    selectedImage?: string;
+}
+export default function ImageViewer({ imageSource, selectedImage }: Props) {
+    const imgSource = selectedImage ? { uri: selectedImage } : imageSource;
     return (
-            <Image source={imageSource} style={styles.image} />
+        <Image source={imgSource} style={styles.image} />
     )
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+    },
 });
